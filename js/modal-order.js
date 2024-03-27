f25156d976fa5036c65bf268546abebf9121b184
Maksym <112901851+Maksym82@users.noreply.github.com>
2024-03-27 22:43:33 +0200

    Update modal

--- js/modal-order.js
+++ js/modal-order.js
@@ -3,6 +3,8 @@
     openBuyModalBtns: document.querySelectorAll('[data-modal-open-buy]'),
     closeBuyModalBtn: document.querySelector('[data-modal-close-buy]'),
     closeModalThankBtn: document.querySelector('[data-modal-close-thankyou]'),
+    closeModalContinue: document.querySelector('[data-modal-close-continue]'),
+    openModalOrderFlowers: document.querySelector('[data-modal-order-flowers]'),
     submitBuyBtn: document.querySelector('[data-modal-order-submit]'),
     buyModal: document.querySelector('[data-modal-buy]'),
     thankModal: document.querySelector('[data-top-sellers-thankyou]'),
@@ -11,6 +13,8 @@
   refs.openBuyModalBtns.forEach(addToggleEvent);
   refs.closeBuyModalBtn.addEventListener('click', toggleBuyModal);
   refs.closeModalThankBtn.addEventListener('click', toggleThankModal);
+  refs.closeModalContinue.addEventListener('click', toggleThankModal);
+  refs.openModalOrderFlowers.addEventListener('click', toggleBuyModal);
   refs.submitBuyBtn.addEventListener('click', closeBuyModalAndShowThankyou);
 
   function toggleBuyModal() {
